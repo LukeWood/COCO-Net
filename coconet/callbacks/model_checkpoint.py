5904df1bbb7621c272bb7025d7b229107d4ee525
ayulockin <mein2work@gmail.com>
2022-10-11 14:14:02 +0000

    document model checkpoint callback

--- coconet/callbacks/model_checkpoint.py
+++ coconet/callbacks/model_checkpoint.py
@@ -5,6 +5,16 @@ import wandb.keras import WandbModelCheckpoint
 
 
 def get_model_checkpoint_callback(args):
+    """
+    Use this function to initialize a model checkpointing callback.
+    If wandb is used, `WandbModelCheckpoint` will be used that uploads
+    the checkpoints to W&B Artifacts otherwise, TensorFlow's
+    `ModelCheckpoint` is used.
+
+    Arguments:
+        args (ml_collections.ConfigDict): pass top-level config dict.
+
+    """
     if wandb.run is not None:
         model_checkpointer = WandbModelCheckpoint(
             filepath=args.callback_config.checkpoint_filepath,
